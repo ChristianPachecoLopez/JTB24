16b34dc36a671675596376283442509df4650e4c
ChristianPachecoLopez <christian.pachecolopez@hotmail.com>
2024-02-17 16:41:21 +0100

    update

---
+++ src/tutorial10.java
@@ -0,0 +1,20 @@
+
+public class tutorial10 {
+
+	public static void main(String[] args) {
+		int loop = 0;
+
+		while (true) {
+			System.out.println("Looping: " + loop);
+
+			if (loop == 5) {
+				break;
+			}
+
+			loop++;
+
+			System.out.println("Running");
+		}
+	}
+
+}
