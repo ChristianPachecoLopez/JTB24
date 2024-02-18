5f6ff43ea51be5b2c7163b0e0609c800beb19d9b
ChristianPachecoLopez <christian.pachecolopez@hotmail.com>
2024-02-18 17:53:20 +0100

    update

---
+++ src/tutorial25.java
@@ -0,0 +1,33 @@
+class Frog2 {
+
+	private int id;
+	private String name;
+
+	public Frog2(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String toString() {
+
+		return String.format("%-4d: %s", id, name);
+
+		/*
+		 * StringBuilder sb = new StringBuilder();
+		 * sb.append(id).append(": ").append(name);
+		 * 
+		 * return sb.toString();
+		 */
+	}
+}
+
+public class tutorial25 {
+
+	public static void main(String[] args) {
+		Frog2 frog1 = new Frog2(7, "Freddy");
+		Frog2 frog2 = new Frog2(5, "Roger");
+
+		System.out.println(frog1);
+		System.out.println(frog2);
+	}
+}
